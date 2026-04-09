7b2d2b79407074a032065964e2555df047a9c31a
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-10 00:49:39 +0530

    feat: update Azure resources including resource group, container registry, and DNS zone

--- imports.tf
+++ imports.tf
@@ -4,11 +4,6 @@ import {
 }
 
 import {
-  to = azurerm_container_app_environment.spiritops_container_app_env
-  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env"
-}
-
-import {
   to = azurerm_container_registry.spiritops
   id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.ContainerRegistry/registries/spiritops"
 }
--- main.tf
+++ main.tf
@@ -3,20 +3,11 @@ resource "azurerm_resource_group" "migrate_scope" {
   location = "southindia"
 }
 
-resource "azurerm_container_app_environment" "spiritops_container_app_env" {
-  name                = "spiritops-container-app-env"
-  resource_group_name = azurerm_resource_group.migrate_scope.name
-  location            = "southindia"
-
-  log_analytics_workspace_id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.OperationalInsights/workspaces/workspaceaicloudbuilder9db5"
-}
-
 resource "azurerm_container_registry" "spiritops" {
-  name                = "spiritops"
-  resource_group_name = azurerm_resource_group.migrate_scope.name
-  location            = "southindia"
-  sku                 = "Basic"
-
+  name                          = "spiritops"
+  resource_group_name           = azurerm_resource_group.migrate_scope.name
+  location                      = "southindia"
+  sku                           = "Basic"
   admin_enabled                 = true
   public_network_access_enabled = true
 }
@@ -25,12 +16,12 @@ resource "azurerm_log_analytics_workspace" "workspaceaicloudbuilder9db5" {
   name                = "workspaceaicloudbuilder9db5"
   resource_group_name = azurerm_resource_group.migrate_scope.name
   location            = "southindia"
-
-  sku              = "PerGB2018"
-  retention_in_days = 30
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
 resource "azurerm_dns_zone" "spiritops_in" {
   name                = "spiritops.in"
   resource_group_name = azurerm_resource_group.migrate_scope.name
+  location            = "global"
 }
