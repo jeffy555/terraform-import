623e1576d87146dd91329e359c1c282ddfb57513
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-10 20:14:54 +0530

    feat: update infrastructure with resource group, container registry, and DNS zone

--- main.tf
+++ main.tf
@@ -1,6 +1,23 @@
 resource "azurerm_resource_group" "migrate_scope" {
   name     = "AICloudBuilder"
   location = "southindia"
+  tags = {
+    ManagedBy       = "MigrateOps"
+    MigrateOpsImport = "true"
+  }
+}
+
+resource "azurerm_container_registry" "spiritops" {
+  name                     = "spiritops"
+  resource_group_name      = azurerm_resource_group.migrate_scope.name
+  location                 = "southindia"
+  sku                      = "Basic"
+  admin_enabled            = true
+  public_network_access_enabled = true
+  tags = {
+    ManagedBy       = "MigrateOps"
+    MigrateOpsImport = "true"
+  }
 }
 
 resource "azurerm_log_analytics_workspace" "workspaceaicloudbuilder9db5" {
@@ -9,21 +26,6 @@ resource "azurerm_log_analytics_workspace" "workspaceaicloudbuilder9db5" {
   location            = "southindia"
   sku                 = "PerGB2018"
   retention_in_days   = 30
-
-  tags = {
-    ManagedBy       = "MigrateOps"
-    MigrateOpsImport = "true"
-  }
-}
-
-resource "azurerm_container_registry" "spiritops" {
-  name                          = "spiritops"
-  resource_group_name           = azurerm_resource_group.migrate_scope.name
-  location                      = "southindia"
-  sku                           = "Basic"
-  admin_enabled                 = true
-  public_network_access_enabled = true
-
   tags = {
     ManagedBy       = "MigrateOps"
     MigrateOpsImport = "true"
@@ -33,7 +35,7 @@ resource "azurerm_container_registry" "spiritops" {
 resource "azurerm_dns_zone" "spiritops_in" {
   name                = "spiritops.in"
   resource_group_name = azurerm_resource_group.migrate_scope.name
-
+  location            = "global"
   tags = {
     ManagedBy       = "MigrateOps"
     MigrateOpsImport = "true"
